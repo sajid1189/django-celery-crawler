ee5c98782abfe3d58d7782a762c5c49e98f703a6
Sajidur Rahman <sajidur89@gmail.com>
2018-05-06 16:20:52 +0200

    get param renamed.

--- crawler/views.py
+++ crawler/views.py
@@ -13,14 +13,14 @@ from .models import OutLink
 def get_outlinks(request):
     links = []
     result_size = 100
-    len_param = request.GET.get('len')
+    len_param = request.GET.get('qs')
     print len_param
     if len_param and len_param > 0:
         result_size = len_param
     try:
         with transaction.atomic():
             outlinks = OutLink.objects.filter(download_status=OutLink.DownloadStatus.Available)
-            if outlinks.count() > result_size:
+            if outlinks.count() < result_size:
                 outlinks = outlinks[:result_size]
             for link in outlinks:
                 links.append(link.url)
