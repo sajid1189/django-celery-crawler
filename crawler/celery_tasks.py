ae4eb116d59c58d3437556c38ebf44ec0776acd0
Sajidur Rahman <sajidur89@gmail.com>
2018-05-07 16:41:43 +0200

    user agents included in header.

--- crawler/celery_tasks.py
+++ crawler/celery_tasks.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import random
 from urlparse import urlparse
 from celery import Celery
 
@@ -21,6 +22,25 @@ app = Celery('crawler', broker="amqp://localhost", backend='rpc://localhost')
 #     os.chdir('/Users/sajidur/Desktop')
 #     time.sleep(l[x])
 #     print x, ' finished'
+
+user_agents = ["Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
+               "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+               "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
+               "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+               "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
+               "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0",
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0",
+               "Mozilla/5.0 (Windows; U; Windows NT 6.1; tr-TR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+               "Mozilla/5.0 (Windows; U; Windows NT 6.1; ko-KR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A",
+               "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+               "Opera/9.80 (X11; Linux i686; U; hu) Presto/2.9.168 Version/11.50",
+               "Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14"
+               "Opera/9.80 (X11; Linux x86_64; U; Ubuntu/10.10 (maverick); pl) Presto/2.7.62 Version/11.01"
+               ]
 
 
 @app.task
@@ -52,15 +72,16 @@ def downloader(url, tor=True):
         except Page.DoesNotExist:
 
             try:
+                session = requests.session()
+                session.headers = {'User-Agent': random.random*len(user_agents)}
                 if tor:
-                    session = requests.session()
                     session.proxies = {
                         'http': 'socks5h://localhost:9050',
                         'https': 'socks5h://localhost:9050'
                     }
                     response = session.get(url)
                 else:
-                    response = requests.get(url)
+                    response = session.get(url)
                 if response.status_code == 200:
                     soup = Soup(url, response)
                     if soup.soup:
@@ -68,6 +89,7 @@ def downloader(url, tor=True):
                         outlink_obj.save()
                         try:
                             Page.objects.using('pages').create(url=url, content=soup.html)
+                            print 'Page object created {}'.format(url)
                         except Exception as e:
                             print "page object could not be created: {}".format(url)
 
@@ -83,7 +105,7 @@ def downloader(url, tor=True):
 
                             except OutLink.DoesNotExist:
                                 outlink = OutLink.objects.create(url=outlink)
-                                print 'created {}'.format(outlink)
+                                print 'outlink created {}'.format(outlink)
 
                             except Exception as e:
                                 print e
